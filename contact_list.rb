d0285898090aad87fe43de8683f3bc6802654176
Paul Iogna <paul.iogna@gmail.com>
2016-01-11 13:59:48 -0500

    Command 1 completed

--- contact_list.rb
+++ contact_list.rb
@@ -1,7 +1,17 @@
+require_relative 'contact'
+
+# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
-  def initialize(args)
 
-  end
+  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+end
+if ARGV.length == 0
+  puts "Here is a list of available commands"
+  puts "\tnew      - Create a new contact"
+  puts "\tlist     - List all contacts"
+  puts "\tshow     - Show a contact"
+  puts "\tsearch   - Search contacts"
 
+  exit
 end
